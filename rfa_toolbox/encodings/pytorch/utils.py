8d0112d362cf3719adf6022c95b2cfe4a336f9c0
MLRichter <matrichter@uos.de>
2022-01-28 15:47:20 +0100

    fix: added a simple way to add additional handlers

--- rfa_toolbox/encodings/pytorch/utils.py
+++ rfa_toolbox/encodings/pytorch/utils.py
@@ -1,4 +1,10 @@
 import warnings
+from collections import Callable
+
+import torch
+
+from rfa_toolbox.encodings.pytorch.intermediate_graph import RESOLVING_STRATEGY
+from rfa_toolbox.encodings.pytorch.layer_handlers import GenericLayerTypeHandler
 
 
 def toggle_coerce_torch_functional(
@@ -14,7 +20,7 @@ def toggle_coerce_torch_functional(
     Args:
         coerce:         True by default, enabled or disables a raise-condition
                         whenever a call from torch.nn.functional is encountered
-        kenrel_size:    kernel size of the convolution, this is a default value
+        kernel_size:    kernel size of the convolution, this is a default value
                         to be assumed for all_functional
                         layers. Default is a kernel_size of 1,
                         since it does not change the receptive field expansion.
@@ -29,3 +35,33 @@ def toggle_coerce_torch_functional(
         "you may remove it from your code",
         DeprecationWarning,
     )
+
+
+def add_custom_layer_handler(
+    class_name: str,
+    name_handler: Callable[[torch.nn.Module, str], str] = lambda x, y: y,
+    kernel_size_provider: Callable[[torch.nn.Module], int] = lambda x: 1,
+    stride_size_provider: Callable[[torch.nn.Module], int] = lambda x: 1,
+    filters_provider: Callable[[torch.nn.Module], int] = lambda x: None,
+    units_provider: Callable[[torch.nn.Module], int] = lambda x: None,
+) -> None:
+    """Add a custom layer handler to the list of handlers.
+
+    Args:
+        name_handler:           function that returns the name of the layer
+        class_name:             name of the class that is handled by the handler
+        kernel_size_provider:   function that returns the kernel size of the layer
+        stride_size_provider:   function that returns the stride size of the layer
+        filters_provider:       function that returns the number of filters of the layer
+        units_provider:         function that returns the number of units of the layer
+    """
+    class_name = class_name.lower()
+    handler = GenericLayerTypeHandler(
+        class_name=class_name,
+        name_provider=name_handler,
+        kernel_size_provider=kernel_size_provider,
+        stride_size_provider=stride_size_provider,
+        filters_provider=filters_provider,
+        units_provider=units_provider,
+    )
+    RESOLVING_STRATEGY.insert(0, handler)
