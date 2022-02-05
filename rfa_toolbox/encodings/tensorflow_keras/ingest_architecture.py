6505166f5283ce5e053df6a00b8b1e9c3e367b17
MLRichter <matrichter@uos.de>
2022-02-05 20:30:05 +0100

    fix: kernel size one can be ignored. Keras now recognizes DepthWiseSeperable convolutions correctly as convolutions

--- rfa_toolbox/encodings/tensorflow_keras/ingest_architecture.py
+++ rfa_toolbox/encodings/tensorflow_keras/ingest_architecture.py
@@ -176,9 +176,6 @@ def create_graph_from_model(
                 By default, not filtering is done.
     """
     model_dict = keras_model_to_dict(model)
-    from pprint import pprint
-
-    pprint(model_dict)
     callable_filter = (
         filter_rf
         if (not isinstance(filter_rf, str) and filter_rf is not None)
